36875c3b27974ff2d494492d867e9cbba17acf88
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-15 04:36:51 +0100

    feat: add book model and author relationship

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 
@@ -8,6 +8,21 @@ class Base(DeclarativeBase):
 
 class Author(Base):
     __tablename__ = "authors"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
+    books: Mapped[list["Book"]] = relationship(
+    back_populates="author"
+)
+
+class Book(Base):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    author_id: Mapped[int] = mapped_column(
+        ForeignKey("authors.id"),
+        nullable=False,
+    )
+    author: Mapped["Author"] = relationship(
+    back_populates="books"
+)
